6cc0c7c1e78c74807f48e1789466c36a6b72b752
7112003sp <bsp71120033@gmail.com>
2024-09-25 17:11:52 +0530

    Create SOSResponse.java

---
+++ SOSResponse.java
@@ -0,0 +1,15 @@
+package com.example.rescuenet;
+
+public class SOSResponse {
+    private String status;
+    private String message;
+
+    // Getters and setters
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
